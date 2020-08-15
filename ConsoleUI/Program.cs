ca147a622d41aafbb58b0c852b3f1226b803e538
Pengfang Liu <lpf_mart@163.com>
2020-08-15 16:35:26 +0800

    添加项目文件。

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
